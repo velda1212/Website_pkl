3cd40dadc754bf21355d5f07f3a2912c4b2aa158
velda1212 <119835790+velda1212@users.noreply.github.com>
2023-01-18 14:23:54 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -15,43 +15,209 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+    const ina = document.getElementById('ina')
+    ina.addEventListener('click', ()=> {
+        Swal.fire({
+            title: "Pekerjaan 2",
+            text:``
+        })
+    })
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    const ica = document.getElementById('ica')
+    ica.addEventListener('click', ()=> {
+        Swal.fire({
+            title: "Pekerjaan 1",
+            html:`<div style="text-align: justify">
+            <p>
+                    A.Jenis kegiatan di tempat pkl
+                    <br>
+                    1. Briefing bersama leader & membaca doa
+                    <br>
+                    2. Packing Barang
+                    <br>
+                    3. Sortir Barang 
+                    <br>
+                    <br>
+                  B.Langkah - Langkah Kerja
+                  <br>
+                    1. Sediakan Box utama produk yang akan dikirim
+                    <br>
+                    2. Susun barang yang sudah di sortir sebanyak 50pcs
+                    <br>
+                    3. Lipat atas plastik hingga rapih 
+                    <br>
+                    4. Selanjutnya lakban ujung yang sudah di lipat
+                    <br>
+                    5. Masukan barang ke dalam Box dan siap di kirim
+                    </p>
+                    </div>`
+        })
+    })
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
+    const velda = document.getElementById('velda')
+    velda.addEventListener('click', ()=> {
+        Swal.fire({
+            title: "Pekerjaan 3",
+            html:`<div style="text-align: justify">
+            <p>
+                    A.Jenis kegiatan di tempat pkl
+              <br>
+                    1. Packing Barang
+                      <br>
+                    2. Sortir Barang 
+                      <br>
+                      <br>
+                  B.Langkah - Langkah Kerja
+                    <br>
+                    1. Sediakan Box utama produk yang akan dikirim
+                      <br>
+                    2. Susun barang yang sudah di sortir sebanyak 50pcs
+                      <br>
+                    3. Lipat atas plastik hingga rapih 
+                      <br>
+                    4. Selanjutnya lakban ujung yang sudah di lipat
+                      <br>
+                    5. Masukan barang ke dalam Box dan siap di kirim
+                    </p>
+                    </div>`
+        })
+    })
+
+     const puji = document.getElementById('puji')
+    puji.addEventListener('click', ()=> {
+        Swal.fire({
+            title: "Pekerjaan 4",
+            html:`<div style="text-align: justify">
+            <p>
+                   1. Meminta Tanda Tangan Merapihkan surat jalan yang sudah melalui pengecekan QC dan sudah mendapatkan tanda tangan departemen PPIC. Kemudian sesegera mungkin menuju ruangan General Manager Production lalu meminta tanda tangan agar bisa melakukan pengiriman barang.
+                    <br>
+                   2.Fotocopy Jika copy-an berkas label ditempat penyimpanan habis, harus cepat-cepat mengcopy lagi dengan berkas label yang asli karena label tersebut akan langsung digunakan oleh operator produksi.
+                    <br>
+                   3. Packing Barang Menyiapkan plastik yang sesuai dengan quantity yang tertera dilabel barang. Memakai sarung tangan yang disediakan lalu mulai memasukkan barang kedalam plastik, jika sudah sesuai dengan quantitybarang tersebut kemudian rapatkan plastik dengan staples atau lakban.
+                    <br>
+                   4. File Surat Jalan Memilah dan memisahkan surat jalan masuk atau keluar satu persatu, lalu bolongkan pinggiran surat jalan menggunakan pembolong kertas, kemudian mencari bantexyang terdapat nama perusahaan yang terkait dan memasukkan file tersebut kedalam bantex.
+                    </p>
+                    </div>`
+        })
+    })
+        const fotoica = document.getElementById('fotoica')
+        fotoica.addEventListener('click' , () => {
+        Swal.fire({
+            title: 'Profile Ica',
+            html: `<div style="text-align: left;line-height: 2;">
+            <p>
+
+
+Nama : Nazwa Ica Mulani
+<br>
+Nis : 202110307
+<br>
+Kelas : XII Rekayasa Perangkat Lunak 1
+<br>
+Agama : Islam
+<br>
+Tempat Tanggal Lahir : Bekasi,23-Mei-2005
+<br>
+Alamat Tempat Tinggal :Jln. Yayasan Nurul Huda Rt06/RW02.
+<br>
+Tempat Prakerin : PT. BINTANG MATRIX INDONESIA
+<br>
+Mulai Prakerin : 18 January 2022
+<br>
+Selesai Prakerin : 28 April 2022
+            </p>
+            </div>`
+        })
+    })
+
+    const fotopelda = document.getElementById('fotovelda')
+    fotopelda.addEventListener('click' , () => {
+        Swal.fire({
+            title: 'Profile Velda',
+            html: `<div style="text-align: left;line-height: 2;">
+            <p>
+Nama : Velda Feronita
+<br>
+Nis : 202110296
+<br>
+Kelas : XII Rekayasa Perangkat Lunak 1
+<br>
+Agama : Islam
+<br>
+Tempat Tanggal Lahir : Bekasi,15-February-2005
+<br>
+Alamat Tempat Tinggal : Jln. Lingkar Bambu,Ciketing Udik Rt:02/Rw03 CiketingUdik,Bantargebang,Kota Bekasi.
+<br>
+Tempat Prakerin : PT. BINTANG MATRIX INDONESIA
+<br>
+Mulai Prakerin : 18 January 2022
+<br>
+Selesai Prakerin : 28 April 2022
+            </p>
+            </div>`
+        })
+    })
+})
+
+        const fotoina = document.getElementById('fotoina')
+        fotoina.addEventListener('click' , () => {
+        Swal.fire({
+            title: 'Profile Ina',
+            html: `<div style="text-align: left;line-height: 2;">
+            <p>
+
+Nama : Ina Rostiani
+<br>
+Nis : 202110298
+<br>
+Kelas : XII Rekayasa Perangkat Lunak 1
+<br>
+Agama : Islam
+<br>
+Tempat Tanggal Lahir : Kuningan,02-Mei-2005
+<br>
+Alamat Tempat Tinggal : Jl. Pangkalan 5,Rt002/Rw005,Ciketing udik.
+<br>
+Tempat Prakerin : PT. BINTANG MATRIX INDONESIA
+<br>
+Mulai Prakerin : 14 February 2022
+<br>
+Selesai Prakerin : 14 Mei 2022
+            </p>
+            </div>`
+        })
+
+
+        const fotofuji = document.getElementById('fotofuji')
+        fotofuji.addEventListener('click' , () => {
+        Swal.fire({
+            title: 'Profile Fuji',
+            html: `<div style="text-align: left;line-height: 2;">
+            <p>
+
+
+
+Nama : Puji Wahyuningtyas
+<br>
+Nis : 202110351
+<br>
+Kelas : XII Rekayasa Perangkat Lunak 2
+<br>
+Agama : Islam
+<br>
+Tempat Tanggal Lahir : Bekasi,21-Juni-2005
+<br>
+Alamat Tempat Tinggal : Ciketing Udik, Bantar Gebang, Bekasi Rt:03/Rw03
+<br>
+Tempat Prakerin : PT. BINTANG MATRIX INDONESIA
+<br>
+Mulai Prakerin : 24 February 2022
+<br>
+Selesai Prakerin : 21 Mei 2022
+            </p>
+            </div>`
+        })
+
+    })
+
+    })
